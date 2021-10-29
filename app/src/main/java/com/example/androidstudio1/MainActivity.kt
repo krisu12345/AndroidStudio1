c26a7e92f01e58ce0532986a74ff3434f315e197
krisu12345 <krzysiu.offical@wp.pl>
2021-10-29 15:54:28 +0200

    swieze

--- app/src/main/java/com/example/androidstudio1/MainActivity.kt
+++ app/src/main/java/com/example/androidstudio1/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.androidstudio1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        przod.setOnClickListener {
+            imageView.setImageResource(R.drawable.cool1);
+            if(
+        }
+        tyl.setOnClickListener {
+            imageView.setImageResource(R.drawable.nibb4);
+        }
     }
 }
