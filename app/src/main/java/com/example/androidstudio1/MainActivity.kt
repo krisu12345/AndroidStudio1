59b46711696983d36c9cc30f07b46ea0920af79b
Krzysztof Niedźwiedzki <K.Niedzwiedzki@student.zschie.edu.pl>
2021-10-31 14:39:09 +0100

    11111

--- app/src/main/java/com/example/androidstudio1/MainActivity.kt
+++ app/src/main/java/com/example/androidstudio1/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.example.androidstudio1
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -9,43 +15,76 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+
+        fota.isEnabled = false
+        if(ActivityCompat.checkSelfPermission(this,Manifest.permission.CAMERA)!=PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA),111)
+        }
+        else
+            fota.isEnabled = true
+        fota.setOnClickListener {
+            val i = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(i, 101)
+        }
+
         var img = "cool1"
 
         przod.setOnClickListener {
             if(img == "cool1"){
-                imageView.setImageResource(R.drawable.grzechu2);
+                imageView.setImageResource(R.drawable.grzechu2)
                 img = "grzechu2"
             }
             else if(img == "grzechu2"){
-                imageView.setImageResource(R.drawable.grzes3);
+                imageView.setImageResource(R.drawable.grzes3)
                 img = "grzes3"
             }
             else if(img == "grzes3"){
-                imageView.setImageResource(R.drawable.nibb4);
+                imageView.setImageResource(R.drawable.nibb4)
                 img = "nibb4"
             }
             else if(img == "nibb4"){
-                imageView.setImageResource(R.drawable.cool1);
+                imageView.setImageResource(R.drawable.cool1)
                 img = "cool1"
             }
         }
         tyl.setOnClickListener {
             if(img == "cool1"){
-                imageView.setImageResource(R.drawable.nibb4);
+                imageView.setImageResource(R.drawable.nibb4)
                 img = "nibb4"
             }
             else if(img == "grzechu2"){
-                imageView.setImageResource(R.drawable.cool1);
+                imageView.setImageResource(R.drawable.cool1)
                 img = "cool1"
             }
             else if(img == "grzes3"){
-                imageView.setImageResource(R.drawable.grzechu2);
+                imageView.setImageResource(R.drawable.grzechu2)
                 img = "grzechu2"
             }
             else if(img == "nibb4"){
-                imageView.setImageResource(R.drawable.grzes3);
+                imageView.setImageResource(R.drawable.grzes3)
                 img = "grzes3"
             }
         }
     }
-}
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == 101)
+        {
+            val pic:Bitmap? = data?.getParcelableExtra<Bitmap>("data")
+                imageView.setImageBitmap(pic)
+        }
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == 111 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            fota.isEnabled = true
+        }
+    }
+
+    }
